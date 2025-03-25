0cdd491d226e9cf69835d3ce3e8ad0638d9ee180
VikeTus0201 <letuantu0123@gmail.com>
2025-03-25 18:14:56 +0700

    add story + subheadline

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
         Karla: ['Karla', 'sans-serif'],
       },
       colors:{
-        'light-coffee':'#C89F94'
+        'light-coffee': '#E8D6D0',
+        'coffee':{
+          50:'#E8D6D0',
+          200:'#C89F94',
+          400:'#A25F48',
+          600:'#744838'
+        }
+        
       },
       keyframes:{
         slideDown:{
